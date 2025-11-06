d3aa0b93e328f4c30d8c91a6d57ab53625b7b5c3
Raul Gouvea <raulgouvea@gmail.com>
2025-11-06 14:41:28 -0300

    feat: adicionar middleware de autenticação com JWT

---
+++ WebApi/src/middlewares/authMiddlewares.ts
@@ -0,0 +1,38 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+interface TokenPayload {
+  userId: string;
+  role: string;
+}
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      return res.status(401).json({ message: "Token de acesso não fornecido" });
+    }
+
+    const token = authorization.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({ message: "Token mal formatado" });
+    }
+
+    const ACCESS_SECRET = process.env.JWT_ACCESS_SECRET;
+    if (!ACCESS_SECRET) {
+      throw new Error("Segredo JWT de acesso não configurado no .env");
+    }
+
+    const payload = jwt.verify(
+      token,
+      ACCESS_SECRET as jwt.Secret
+    ) as TokenPayload;
+
+    next();
+
+  } catch (error) {
+    console.error("Erro de autenticação:", error);
+    return res.status(401).json({ message: "Token inválido ou expirado" });
+  }
+};
--- WebApi/src/routes/index.ts
+++ WebApi/src/routes/index.ts
@@ -2,26 +2,26 @@ import { Router } from "express";
 import { ExampleController } from "../controllers/ExampleController";
 import { AuthController } from "../controllers/AuthController";
 import { UserController } from "../controllers/UserController";
+import { authMiddleware } from "../middlewares/authMiddlewares";
 
 
 
 const router = Router();
 
+//Rotas públicas
 router.get("/health", ExampleController.health);
-
-//Rotas de Auth
 router.post("/login", AuthController.login);
 router.post("/refresh", AuthController.refreshToken);
 router.post("/logout", AuthController.logout);
 
 
-
-//Rotas USUARIOS
-router.post("/users", UserController.createUser);
-router.get("/users", UserController.listUsers);
-router.get("/users/:id", UserController.getUserById);
-router.patch("/users/:id", UserController.updateUser);
-router.delete("/users/:id", UserController.deleteUser);
+//Rotas Protegidas
+// USUARIOS
+router.post("/users", authMiddleware, UserController.createUser);
+router.get("/users", authMiddleware, UserController.listUsers);
+router.get("/users/:id", authMiddleware, UserController.getUserById);
+router.patch("/users/:id", authMiddleware, UserController.updateUser);
+router.delete("/users/:id", authMiddleware, UserController.deleteUser);
 
 
 export default router;
